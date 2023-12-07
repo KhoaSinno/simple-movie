52b8aacbee62996a3b4b6eb1e8f177938b6903ce
KhoaSN <khoasinno@gmail.com>
2023-12-07 14:11:32 +0700

    banner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10 font-medium mb-10">
+        <span className='text-primary'>Home</span>
+        <span className=''>Movies</span>
       </header>
-    </div>
+      <section className='banner h-[500px] page-container'>
+        <div className="w-full h-full rounded-lg relative">
+          <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
+          <img src="https://storage.googleapis.com/leep_app_website/2021/06/End-Game-01.jpg" alt=""
+            className='h-full w-full object-cover rounded-lg'
+          />
+          <div className="absolute left-5 bottom-5 w-full text-white">
+            <h2 className='font-bold text-4xl mb-8'>Advengers: Endgame</h2>
+            <div className="flex items-center gap-x-3 mb-7">
+              <span className='py-2 px-4 border border-white rounded-md'>Advengers</span>
+              <span className='py-2 px-4 border border-white rounded-md'>Advengers</span>
+              <span className='py-2 px-4 border border-white rounded-md'>Advengers</span>
+            </div>
+            <button className='font-medium px-6 py-3 bg-primary rounded-lg'>Watch Now!</button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['DM Sans', 'sans-serif']
+      },
+      colors: {
+        primary: '#F62682'
+      }
+    },
+  },
+  plugins: [],
+}
